7efd568d966106c47ddd4e37f1c370fae765cde3
Valerii Iushchenko <viushchenko@fairfaxdigital.com.au>
2012-03-09 10:34:36 +1100

    PATH for Fairfax

---
+++ source/50_path.sh
@@ -0,0 +1,15 @@
+if [[ -d ~/.gem/ruby/1.8/bin ]] ; then
+
+    PATH=~/.gem/ruby/1.8/bin:$PATH
+fi
+
+if [[ -d /Applications/SenchaSDKTools-2.0.0-Beta ]] ; then 
+
+    sencha=/Applications/SenchaSDKTools-2.0.0-Beta
+    PATH=$sencha:$sencha/command:$sencha/jsbuilder:$PATH
+fi
+
+if [[ -d ~/projects/ffx-mobile-web-framework/ ]] ; then
+
+    PATH=~/projects/ffx-mobile-web-framework/scripts/:$PATH
+fi
