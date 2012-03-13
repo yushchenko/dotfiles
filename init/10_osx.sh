ae0554877411f775a4f5a3ed652368e56a16874b
Valerii Iushchenko <viushchenko@fairfaxdigital.com.au>
2012-03-13 12:08:49 +1100

    rbenv and bundler

--- init/10_osx.sh
+++ init/10_osx.sh
@@ -15,7 +15,7 @@ fi
 
 # Install Homebrew recipes.
 if [[ "$(type -P brew)" ]]; then
-  recipes=(git node tree sl lesspipe id3tool nmap git-extras)
+  recipes=(git node tree sl lesspipe id3tool nmap git-extras rbenv ruby-build)
 
   list="$(to_install "${recipes[*]}" "$(brew list)")"
   if [[ "$list" ]]; then
@@ -24,10 +24,11 @@ if [[ "$(type -P brew)" ]]; then
   fi
 
   # Newer OSX XCode comes with an LLVM gcc which rbenv can't use.
-  # source ~/.dotfiles/source/50_rbenv.sh
-  # if [[ ! "$RBENV_CC" && ! "$(brew list | grep -w "gcc")" ]]; then
-  #   e_header "Installing Homebrew-alt gcc recipe"
-  #   echo "Note: this step can take 15+ minutes, but a non-LLVM gcc is required by rbenv."
-  #   skip || brew install https://github.com/adamv/homebrew-alt/raw/master/duplicates/gcc.rb
-  # fi
+  source ~/.dotfiles/source/50_rbenv.sh
+  if [[ ! "$RBENV_CC" && ! "$(brew list | grep -w "gcc")" ]]; then
+    e_header "Installing Homebrew-alt gcc recipe"
+    echo "Note: this step can take 15+ minutes, but a non-LLVM gcc is required by rbenv."
+    skip || brew install https://github.com/adamv/homebrew-alt/raw/master/duplicates/gcc.rb
+  fi
+
 fi
